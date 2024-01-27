5b40bef6b59e994301e72c1c866cfd5d6b8a5914
prajwal89 <prajwalhallale89@gmail.com>
2024-01-27 19:59:13 +0530

    init

---
+++ state-of-cypto.bat
@@ -0,0 +1,6 @@
+@echo off
+start chrome "https://www.coinglass.com/pro/i/RsiHeatMap"
+start chrome "https://coinank.com/liqHeatMapChart"
+start chrome "https://www.coinglass.com/today"
+start chrome "https://www.lookintobitcoin.com/charts/bitcoin-rainbow-chart"
+start chrome "https://alternative.me/crypto/fear-and-greed-index"
